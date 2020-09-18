72a5ff3dc1a67815b7bcddf87c5f1bfc78e7a09c
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2020-09-18 14:46:29 +0900

    HTMLタグを追加

--- index.php
+++ index.php
@@ -3,3 +3,13 @@ for($i=0;$i=10;$i++){
     echo $_GET["a"]."<br>";
 }
 ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>Shikama Project</title>
+</head>
+<body>
+    <h1>Shikama Project</h1>
+</body>
+</html>
