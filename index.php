e9a5702c4ac68cff9d1bff0220712040616ee6f0
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2020-11-08 22:38:09 +0900

    顧客情報登録用フォームのデータチェック

--- index.php
+++ index.php
@@ -23,16 +23,73 @@ if (empty($dbo)) die('Error: データベースに接続できません');
 $html = get_html($view_html);
 if (empty($html)) die('Error: 指定されたファイルが見つかりません');
 
-if (isset($_GET)) {
+var_dump($_GET);
+if (isset($_GET['SEND'])) {
+    $err_flag = false;
     $forms = array();
     foreach ($_GET as $key => $value) {
         $forms[$key] = $value;
     }
 
+    if (!empty($forms['SEI']) && !empty($forms['MEI'])) {
+        $name = $forms['SEI'] . '　' . $forms['MEI'];
+    } else {
+        $err_flag = true;
+    }
+    if (!empty($forms['KANASEI']) && !empty($forms['KANAMEI'])) {
+        $kana = $forms['KANASEI'] . '　' . $forms['KANAMEI'];
+    } else {
+        $err_flag = true;
+    }
+    if (!empty($forms['LIKE'])) {
+        $like = $forms['LIKE'];
+    }
+    if (!empty($forms['BARTHDAY'])) {
+        // 1件の登録がある時はそれ以上の生年月日登録が無いかをチェック
+        // 仕様次第ではキーを続柄にして誕生日を値で持たせる
+        $barthday = array();
+        $i = 0;
+        foreach ($forms['BARTHDAY'] as $value) {
+            $brelation = $forms['RBARTHDAY'][$i];
+            $barthday += array($i => array($value, $brelation));
+            $i++;
+        }
+    }
+    echo "<hr>誕生日データ(可変長なので表示して確認)<br>";
+    var_dump($barthday);
+    echo "<hr>";
+
+    // 来店情報
+    // 人数
+    if (!empty($forms['NUMBER'])) {
+        $number = $forms['NUMBER'];
+    } else {
+        $err_flag = true;
+    }
+    // 続柄
+    if (!empty($forms['RELATION'])) {
+        $relation = $forms['RELATION'];
+    }
+    // 食事内容
+    if (!empty($forms['EATS'])) {
+        $eats = $forms['EATS'];
+    }
+
+    echo "<hr>変数内容確認<br>";
+    echo "name = $name<br>";
+    echo "kana = $kana<br>";
+    echo "like = $like<br>";
+    var_dump($barthday); echo "<br>";
+    echo "number = $number<br>";
+    echo "relation = $relation<br>";
+    echo "eats = $eats<br>";
+    echo "<hr>";
+
+    
     $sql = "SELECT * FROM shikama.clients";
     $res = $dbo->query($sql);
-    var_dump($res);
-    echo "クエリ実行<br>";
+
+    if ($err_flag) echo "入力項目にエラーがあります";
 
     echo $html;
 
@@ -47,7 +104,7 @@ $dbo = null;
 
 function insert_clients($forms) {
     var_dump($forms);
-    
+
 }
 
 
