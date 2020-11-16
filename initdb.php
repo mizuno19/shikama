56ac07b696fba3418096846de4185321e761745d
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2020-11-16 11:25:50 +0900

    データベース操作に関する処理を更新

--- initdb.php
+++ initdb.php
@@ -2,19 +2,6 @@
 require_once 'config.php';
 require_once 'lib/dblib.php';
 
-$db_dsn = [
-    'dsn' => "mysql:dbname=" . Config::DB_DBNAME
-                . ";host=" . Config::DB_HOST
-                . ";port=" . Config::DB_PORT
-                . ";charset=" . Config::DB_CHARSET,
-    'user' => Config::DB_USER,
-    'pass' => Config::DB_PASSWD,
-    'opt' => [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_EMULATE_PREPARES => false,    // 静的プレースホルダを使う
-    ]
-];
-
 $dbo = dbconnect($db_dsn);
 if (empty($dbo)) die('Error: データベースに接続できません');
 
@@ -35,17 +22,15 @@ echo <<<"EOH"
 EOH;
 
 
-// テーブルを一度削除
+echo "<hr><p>テーブル削除</p><hr>";
 drop_table($dbo, "電話番号");
 drop_table($dbo, "連絡先区分");
 drop_table($dbo, "生年月日");
 drop_table($dbo, "来店記録");
 drop_table($dbo, "顧客");
 
-echo "<hr>";
-echo "<p>----- テーブル作成 ------</p>";
 
-// テーブルの作成確認
+echo "<hr><p>テーブル作成</p><hr>";
 $sql = "create table 顧客(
     顧客ID char(7) not null,
     姓 varchar(100),
@@ -54,22 +39,22 @@ $sql = "create table 顧客(
     メイ varchar(100),
     備考 text,
     primary key(顧客ID));";
-create_table($dbo, $sql);
+execute($dbo, $sql, true);
 
 $sql = "create table 連絡先区分(
     区分ID char(1) not null,
     区分名 varchar(30) not null,
     primary key(区分ID));";
-create_table($dbo, $sql);
+execute($dbo, $sql, true);
 
 $sql = "create table 電話番号(
     顧客ID char(7) not null,
-    電話番号 varchar(11) not null,
     区分ID char(1) not null,
+    電話番号 varchar(11) not null,
     primary key(顧客ID,区分ID),
     foreign key (顧客ID) references 顧客(顧客ID),
     foreign key (区分ID) references 連絡先区分(区分ID));";
-create_table($dbo, $sql);
+execute($dbo, $sql, true);
 
 $sql = "create table 生年月日(
     顧客ID char(7) not null,
@@ -78,7 +63,7 @@ $sql = "create table 生年月日(
     続柄 varchar(10),
     primary key(顧客ID,登録ID),
     foreign key (顧客ID) references 顧客(顧客ID));";
-create_table($dbo, $sql);
+execute($dbo, $sql, true);
 
 $sql = "create table 来店記録(
     顧客ID char(7) not null,
@@ -87,34 +72,34 @@ $sql = "create table 来店記録(
     続柄 varchar(10),
     メニュー text,
     primary key(顧客ID));";
-create_table($dbo, $sql);
+execute($dbo, $sql, true);
+
+
+echo "<hr><p>初期データ投入</p><hr>";
+$sql = "insert into 連絡先区分 values
+      ('1','自宅')
+    , ('2','会社')
+    , ('3','本人')
+    , ('4','その他')";
+execute($dbo, $sql, true);
+
+
+echo "<hr><p>サンプルデータ</p><hr>";
+$sql = "insert into 顧客 values
+    ('2011121', '出江田', '入太郎', 'デエタ', 'イレタロウ', '好きなもの：肉　食べられないもの：牛');";
+execute($dbo, $sql, true);
+$sql = "insert into 電話番号 values
+      ('2011121', '1', '0123456789')
+    , ('2011121', '3', '08011112222');";
+execute($dbo, $sql, true);
+$sql = "insert into 生年月日 values
+      ('2011121', 1, '1978-01-01', '本人')
+    , ('2011121', 2, '1980-04-28', '妻');";
+execute($dbo, $sql, true);
 
 echo <<<"EOH"
 </body>
 </html>
 EOH;
-
-
-function drop_table($dbo, $table) {
-    $sql = "DROP TABLE ${table};";
-    try {
-        $res = $dbo->query($sql);
-        echo "<p>${table} テーブルを削除しました。</p>";
-    } catch (Exception $e) {
-        echo "<p>${table} テーブルの削除に失敗しました。<br>";
-        echo "&nbsp;&nbsp;(" . $e->getMessage() . ")</p>";
-    }
-}
-
-function create_table($dbo, $sql) {
-    try {
-        $dbo->query($sql);
-        echo "<p>【 SQL実行成功 】</p>";
-        echo "<p>[ ${sql} ]</p>";
-    } catch (Exception $e) {
-        echo "<p>${sql}の実行に失敗しました。<br>";
-        echo "&nbsp;&nbsp;(" . $e->getMessage() . ")</p>";
-    }
-}
 
 ?>
--- lib/dblib.php
+++ lib/dblib.php
@@ -11,4 +11,34 @@ function dbconnect($dsn) {
 }
 
 
+function execute($dbo, $sql, $flag=false) {
+    try {
+        $res = $dbo->query($sql);
+        if ($flag) {
+            echo "<p>【 SQL実行成功 】</p>";
+            echo "<p>[ ${sql} ]</p>";
+        }
+        return $res;
+    } catch (Exception $e) {
+        if ($flag) {
+            echo "<p>${sql}の実行に失敗しました。<br>";
+            echo "&nbsp;&nbsp;(" . $e->getMessage() . ")</p>";
+        }
+        return null;
+    }
+}
+
+function drop_table($dbo, $table) {
+    $sql = "DROP TABLE ${table};";
+    try {
+        $dbo->query($sql);
+        echo "<p>${table} テーブルを削除しました。</p>";
+    } catch (Exception $e) {
+        echo "<p>${table} テーブルの削除に失敗しました。<br>";
+        echo "&nbsp;&nbsp;(" . $e->getMessage() . ")</p>";
+    }
+}
+
+
+
 1;
