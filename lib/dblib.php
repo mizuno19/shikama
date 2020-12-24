efba4b0df2c215691b016199a63bdf53743af885
Katsuhiro Asaka <asaka@chiba-fjb.ac.jp>
2020-12-24 09:21:37 +0900

    来店情報のインサートを関数化

--- lib/dblib.php
+++ lib/dblib.php
@@ -1,5 +1,24 @@
 <?php
 
+// 来店情報インサート
+// $dbo: PDOインスタンス
+// $id: 顧客ID, $date: 来店日時
+// $number: 来店人数, $relation: 続柄, $eats: メニュー内容
+// 来店IDは自動連番による記録
+function insert_visit($dbo, $id, $date, $number, $relation, $eats) {
+    // プリペアードステートメントを使い、インジェクション攻撃対策
+    $sql = "INSERT INTO 来店記録 VALUES(null, :id, :date, :number, :relation, :eats)";
+    $stmt = $dbo->prepare($sql);    // SQLを設定
+
+    // SQL内の名前に変数の値を割り当てる
+    $stmt->bindParam(":id", $id);
+    $stmt->bindParam(":date", $date);
+    $stmt->bindParam(":number", $number);
+    $stmt->bindParam(":relation", $relation);
+    $stmt->bindParam(":eats", $eats);
+    return $stmt->execute();
+}
+
 // データベースへ接続しPDOインスタンスを返す
 function dbconnect($dsn) {
     $pdo = null;
