77965df6b983a0ec848e414b36460223b2888869
水野 望恵 <x19j084@chiba-fjb.ac.jp>
2020-12-25 17:05:00 +0900

    印刷画面の修正という名のプレゼント

--- print.php
+++ print.php
@@ -5,7 +5,6 @@ require_once 'lib/dblib.php';
 $dbo = dbconnect($db_dsn);
 if (empty($dbo)) die('Error: データベースに接続できません');?>
 
-
 <!DOCTYPE html>
 <html lang="ja">
     <head>
@@ -20,54 +19,65 @@ if (empty($dbo)) die('Error: データベースに接続できません');?>
         $n = 0;     //印刷予定枚数
         $i = 0;     //印刷実行数
         $id = $_GET['ID'];
+        $no = $_GET['no'];
         $sql = "SELECT 顧客.顧客ID, 顧客.姓, 顧客.名, 顧客.セイ, 顧客.メイ, 顧客.備考,来店記録.日時,来店記録.メニュー FROM 顧客 left join 来店記録 on 顧客.顧客ID=来店記録.顧客ID where 顧客.顧客ID='${id}' order by 来店記録.日時 desc";
         $res = execute($dbo, $sql); 
         $db_data = $res->fetchAll(PDO::FETCH_ASSOC);
         ?>
         <form>
-        <div  class="noprint center">   
+        <input type="hidden" name="ID" value="<?=$id?>">
+        <div  class="noprint center">
         印刷範囲<br><select name='no'>
             <option value='one'>1回分</option>
             <option value='two'>2回分</option>
             <option value='three'>3回分</option>
             </select></div>
-            <?php if('no'=='one'){ 
+            <?php 
+            if($no=='one'){ 
                 $n=1;
-            }else if('no'=='two'){
+            }else if($no=='two'){
                 $n=2;
-            }else{
+            }else if($no=='three'){
                 $n=3;
-            }?>
-            <?php while($i<$n) {
-                $rem=str_replace(PHP_EOL,"<br>","   ".$db_data[0]["備考"]);
-                ?>
+            }
+            ?>
                 <div class="page">
-                    <div class="prev" id="a<?=$i?>">
-                    <hr>
+                        <div class="prev" id="a<?=$i?>">
+                        <hr>
                         <div class="name">
                         <p><?php echo $db_data[0]["セイ"]." ".$db_data[0]["メイ"] ?>　サマ</p>
                         <p><?php echo $db_data[0]["姓"]." ".$db_data[0]["名"] ?>　様</p>
                         </div>
                     <hr>
                     </div>
+                    <?php 
+                    $rem=str_replace(PHP_EOL,"<br>","   ".$db_data[0]["備考"]);
+                    ?>
                     <div class="rem">
                     <p><?php echo $rem ?></p> <!-- // 改行処理があると良い？ -->  
                     </div>
-                    <hr> 
+                    <hr><?php
+            while($i<$n) {
+                ?>
+                <?php if(!empty($db_data[$i]["日時"])) {?> 
+                    <div class="details">
                     <div class="date">
                     <p><?php echo substr($db_data[$i]["日時"],0,10) ?></p>
                     <p><?php echo substr($db_data[$i]["日時"],11) ?></p>
                     </div>
-                    <p class="menu">
+                    <div class="menu">
                     <?php $menu = explode("、",$db_data[$i]["メニュー"]);
                     $j=1;
                     foreach($menu as $m){
                         echo "$j".".  "."$m<br>";
                         $j++;
                     }
-                    ?></p>
-                </div>
+                    ?></div>
+                    </div>
+                <?php } ?>
+                <hr>
                 <?php $i++; }?>
+                </div>
                 <div  class="center"><input type="submit" value="印刷" class="noprint"></div>
         </form>   
     </body>
--- visit.php
+++ visit.php
@@ -12,7 +12,7 @@ echo <<<"EOH"
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/base.css" type="text/css">
-    <link rel="stylesheet" href="css/index.css" type="text/css">
+    <link rel="stylesheet" href="css/visit.css" type="text/css">
 </head>
 <body>
 <h1>来店情報登録</h1>
@@ -62,33 +62,32 @@ if (!$res) {
 } else {
     if (!isset($_POST['SEND'])) {
         $date = date('Y-m-d H:i:s');
-        echo '<p>' . $res['セイ'] . '　' . $res['メイ'] . '</p>';
-        echo '<p>' . $res['姓'] . '　' . $res['名'] . '様</p>';
+        echo '<div class="name"><p>' . $res['セイ'] . '　' . $res['メイ'] . '</p>';
+        echo '<p>' . $res['姓'] . '　' . $res['名'] . '様</p></div>';
         echo '<form action="" method="POST">';
         echo "<input type=\"hidden\" name=\"ID\" value=\"${id}\">";
-        echo "<p>来店日時";
         echo "<input type=\"button\" value=\"現在時刻取得\" onClick=\"getNow();\">".'<br>';
-        echo "<input type=\"text\" name=\"year\" value=\"$year\" id=\"year\">".'年<br>';
-        echo "<input type=\"text\" name=\"month\" value=\"$month\" id=\"month\">".'月<br>';
-        echo "<input type=\"text\" name=\"day\" value=\"$day\" id=\"day\">".'日<br>';
-        echo "<input type=\"text\" name=\"hour\" value=\"$hour\" id=\"hour\">".'時<br>';
-        echo "<input type=\"text\" name=\"minute\" value=\"$minute\" id=\"minute\">".'分<br>';
+        echo "<p>来店日時:";
+        echo "<input type=\"text\" name=\"year\" value=\"$year\" id=\"year\" size=\"3\">".'年';
+        echo "<input type=\"text\" name=\"month\" value=\"$month\" id=\"month\" size=\"3\">".'月';
+        echo "<input type=\"text\" name=\"day\" value=\"$day\" id=\"day\" size=\"3\">".'日';
+        echo "<input type=\"text\" name=\"hour\" value=\"$hour\" id=\"hour\" size=\"3\">".'時';
+        echo "<input type=\"text\" name=\"minute\" value=\"$minute\" id=\"minute\" size=\"3\">".'分';
         echo "</p>";
         echo "<p>人数：";
-        echo "<input type=\"text\" name=\"NUMBER\" value=\"\" size=\"3\" maxlength=\"2\">";
+        echo "<input type=\"text\" name=\"NUMBER\" value=\"\" size=\"3\" maxlength=\"2\">"."人";
         echo "</p>";
         echo "<p>続柄:";
         echo "<input type=\"text\" name=\"RELATION\" value=\"\" size=\"10\" maxlength=\"20\">";
         echo "</p>";
         echo "<p>食べたもの:";
-        echo "<input type=\"text\" name=\"EATS\" value=\"\" size=\"100\" maxlength=\"200\">";
+        echo "<textarea rows=\"5\" cols=\"50\" name=\"EATS\"></textarea>";
         echo "</p>";
         echo '<p>';
         echo '<input type="submit" name="SEND" value="登録">';
         echo '</p>';
         echo '</form>';
     } else {
-        
         echo "登録処理<br>";
         echo '<a href="./">トップへ</a>';
     }
